8db2173ee41db70bb9b8afe8c65f14fd624298d4
tomasramg <tomasramirezg@gmail.com>
2020-09-24 19:08:21 +0200

    Metadata, enlaces (a los repositorios) y gazapos corregidos.

--- src/mock/data.js
+++ src/mock/data.js
@@ -3,8 +3,8 @@ import { nanoid } from 'nanoid';
 // HEAD DATA
 export const headData = {
   title: 'Tomás Ramírez González', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  lang: 'es', // e.g: en, es, fr, jp
+  description: 'Bienvenido al portfolio del desarrollador web jr. Tomás Ramírez González, donde podrás encontrar soluciones a tus proyectos Full Stack (JavaScript, PHP).', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'tomas.jpg',
-  paragraphOne: 'Soy un programador que ha desarrollado habilidades en HML5, CSS, JavaScript y PHP en el bootcamp de Upgrade Hub, durante la cuarentena causada por el COVID-19.',
+  paragraphOne: 'Soy un programador que ha desarrollado habilidades en HTML5, CSS, JavaScript y PHP en el bootcamp de Upgrade Hub, durante la cuarentena causada por el COVID-19.',
   paragraphTwo: 'Estoy en la búsqueda de una oportunidad de trabajo como desarrollador junior de aplicaciones web. Si estás interesado en que crezcamos juntos, no dudes en consultarme.  ',
   paragraphThree: 'Además ser un desarrollador web, me considero un profesional polivalente y resolutivo que en los últimos años se he dedicado al mundo del marketing digital, específicamente, al sector del ticketing.',
   resume: 'https://www.linkedin.com/in/tomasramirezgonzalez/', // if no resume, the button will not show up
@@ -41,7 +41,8 @@ export const projectsData = [
     title: 'Lucky',
     info: 'Lucky ha sido el proyecto final del Bootcamp de Full Stack Web Develpment que hemos desarrollado en un productivo equipo. Implementamos el diseño que hizo otro equipo del Bootcamp de UX/UI sobre una web de adopción de animales, la cuál tiene muchos elementos como un formulario de candidatos de adopción, un mapa con las mascotas geolocalizadas y un login de usuarios, entre otras funcionalidades.',
     info2: '',
-    repo: 'https://gitlab.com/Durillo/lucky-frontend', // if no repo, the button will not show up
+    url: 'https://gitlab.com/tomasramg/Lucky',
+    repo: 'https://gitlab.com/tomasramg/Lucky', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -49,6 +50,7 @@ export const projectsData = [
     title: 'Maleteo',
     info: 'Para el proyecto final del módulo de PHP del bootcamp de Upgrade Hub, hemos desarrollado la landing page del proyecto Maleteo, una web con la que los usuarios pueden ofrecerse para cuidar el equipaje de los turistas. Para desarrollar la landing page hemos usado Symfony, Twig y MySQL. ',
     info2: '',
+    url: 'https://gitlab.com/tomasramg/maleteo-landing',
     repo: 'https://gitlab.com/tomasramg/maleteo-landing', // if no repo, the button will not show up
   },
 ];
